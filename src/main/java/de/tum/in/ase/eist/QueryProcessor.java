52c1af0a552abdaa8a08feefe606f1053197ddea
Dominik Zehnter <ge32dih@tum.de>
2022-07-15 14:54:28 +0200

    nix

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -6,23 +6,24 @@ import org.springframework.stereotype.Service;
 public class QueryProcessor {
 
     public String process(String query) {
-		query = query.toLowerCase();
+        query = query.toLowerCase();
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        }
-        if (query.contains("what is")) {
-            String[] a = query.split("what is");
-            String[] b = query.split("plus");
-            int ai = Integer.parseInt(a[1]);
-            int bi = Integer.parseInt(b[1]);
-            return String.valueOf(ai + bi);
-
         } else if (query.contains("name")) {
-           return "Dominik";
+            return "Dominik";
         } else { // TODO extend the programm here
+            if (query.contains("what is")) {
+                String[] a = query.split("what is");
+                String[] b = query.split("plus");
+                int ai = Integer.parseInt(a[1]);
+                int bi = Integer.parseInt(b[1]);
+                return String.valueOf(ai + bi);
+
+            }
             return "";
         }
+
     }
 }
