f7eed07a61717910d84c4ab59d1c565b6fc632b3
Dominik Zehnter <ge32dih@tum.de>
2022-07-15 14:52:18 +0200

    nix

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -11,6 +11,14 @@ public class QueryProcessor {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
+        }
+        if (query.contains("what is")) {
+            String[] a = query.split("what is");
+            String[] b = query.split("plus");
+            int ai = Integer.parseInt(a[0]);
+            int bi = Integer.parseInt(b[1]);
+            return String.valueOf(ai + bi);
+
         } else if (query.contains("name")) {
            return "Dominik";
         } else { // TODO extend the programm here
