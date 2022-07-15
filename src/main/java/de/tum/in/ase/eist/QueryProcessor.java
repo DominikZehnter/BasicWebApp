796d309da830ca4b8a56e7576a113918e0b8baab
Dominik Zehnter <ge32dih@tum.de>
2022-07-15 15:10:14 +0200

    nix

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,9 +13,22 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
             return "Dominik";
-        }
+        } else { // TODO extend the programm here
+
+            if (query.contains("what is")) {
+                String[] a = query.split("what is ");
+                String numbers = a[0];
+                String[] b = numbers.split(" plus ");
+                int bi1 = Integer.parseInt(b[0]);
+                int bi = Integer.parseInt(b[1]);
+                return String.valueOf(bi1 + bi);
+
+            }
             return "";
+
         }
+
+    }
 
 
 }
